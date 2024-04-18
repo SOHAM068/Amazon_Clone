e05e0f74e9b1e417a81ffb47915937a6afa5bf3a
SOHAM068 <sg3533@srmist.edu.in>
2024-04-18 06:33:22 +0530

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './Screens/Home'
 import Details from './Screens/Details'
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Home : undefined;
   Details : {product : Products};
 }
@@ -14,8 +14,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home}/>
-        <Stack.Screen name='Details' component={Details}/>
+        <Stack.Screen 
+        name='Home' 
+        component={Home}
+        options={{title: "Trending Products"}}
+        />
+        <Stack.Screen 
+        name='Details' 
+        component={Details}
+        options={{title: "Product Details"}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import ProductItems from '../components/ProductItems'
+import Seperator from '../components/Seperator'
 
-export default function Home() {
+import { Product_List } from '../data/constants'
+
+type productaProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({navigation} : productaProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+      data={Product_List}
+      keyExtractor={(item) => item.id.toString()}
+      ItemSeparatorComponent={Seperator}
+      renderItem={({item}) => (
+        <Pressable
+        onPress={() => {
+          navigation.navigate("Details",{
+            product : item
+          })
+        }}
+        >
+          <ProductItems product={item} />
+        </Pressable>
+      )}
+      />
     </View>
   )
 }
-
-const styles = StyleSheet.create({})
+export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    padding: 12,
+    backgroundColor: '#FFFFFF',
+  },
+})
